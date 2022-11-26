42df5d9305a5a72d3b73f4cfce27534c61a9248a
connorwarme <connor.warme@gmail.com>
2022-11-26 14:10:00 -0700

    Build simple page w/ react

--- task-app/src/App.js
+++ task-app/src/App.js
@@ -1,10 +1,49 @@
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class Input extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <input placeholder='Enter Task'></input>
+    )
+  }
+}
+
+class Submit extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <button type='submit'>Submit</button>
+    )
+  }
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 'empty'
+    }
+  }
+
+  onClick() {
+
+  }
+  render() {
+    return (
+      <form>
+        <Input />
+        <Submit />
+      </form>
+    )
+  }
 }
 
 export default App;
